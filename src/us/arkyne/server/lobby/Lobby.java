343611d9beb2a0e01e8c5e9358ffdb3b1312677a
Connor Christie <chillercraft@hotmail.com>
2015-08-02 09:21:54 -0500

    Added lobbys and minigame lobbys

--- src/us/arkyne/server/lobby/Lobby.java
+++ src/us/arkyne/server/lobby/Lobby.java
@@ -1,8 +1,31 @@
 package us.arkyne.server.lobby;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import us.arkyne.server.loader.Loadable;
 
 public abstract class Lobby implements Loadable
 {
+	public void load()
+	{
+		onLoad();
+	}
 	
+	public void unload()
+	{
+		onUnload();
+	}
+	
+	public void deserialize(Map<String, Object> map)
+	{
+		
+	}
+	
+	public Map<String, Object> serialize()
+	{
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		return map;
+	}
 }
---
+++ src/us/arkyne/server/minigame/MiniGameLobby.java
@@ -0,0 +1,32 @@
+package us.arkyne.server.minigame;
+
+import java.util.Map;
+
+import us.arkyne.server.lobby.Lobby;
+
+public class MiniGameLobby extends Lobby
+{
+	@Override
+	public void onLoad()
+	{
+		
+	}
+
+	@Override
+	public void onUnload()
+	{
+		
+	}
+
+	@Override
+	public void deserialize(Map<String, Object> map)
+	{
+		
+	}
+
+	@Override
+	public Map<String, Object> serialize()
+	{
+		return null;
+	}
+}
